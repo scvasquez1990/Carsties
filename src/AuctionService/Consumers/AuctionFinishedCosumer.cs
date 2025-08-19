9fb969fadee52d0d7240a76ca13622a32e04fb8c
scvasquez24 <ingsergiovas@gmail.com>
2025-08-19 07:43:27 -0500

    End of section 6 - Gateway server, and new consumers

---
+++ src/AuctionService/Consumers/AuctionFinishedCosumer.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using AuctionService.Data;
+using Contracts;
+using AuctionService.Entities;
+
+namespace AuctionService.Consumers
+{
+    public class AuctionFinishedCosumer : IConsumer<AuctionFinished>
+    {
+        private readonly AuctionDbContext _dbContext;
+
+        public AuctionFinishedCosumer(AuctionDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task Consume(ConsumeContext<AuctionFinished> context)
+        {
+            Console.WriteLine("--> Consuming auction finished");
+
+            var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+            if (context.Message.ItemSold)
+            {
+                auction.Winner = context.Message.Winner;
+                auction.SoldAmount = context.Message.Amount;
+            }
+
+            auction.Status = auction.SoldAmount > auction.ReservePrice ? Status.Finished : Status.ReserveNotMet;
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+
+}
---
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers
+{
+    public class BidPlacedConsumer : IConsumer<BidPlaced>
+    {
+        public async Task Consume(ConsumeContext<BidPlaced> context)
+        {
+            Console.WriteLine("--> Cosuming bid placed");
+            var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+            if (auction.CurrentHighBid == null || context.Message.BidStatus.Contains("Accepted") &&
+                context.Message.Amount > auction.CurrentHighBid)
+            {
+                auction.CurrentHighBid = context.Message.Amount;
+                await auction.SaveAsync();
+            }
+
+        }
+    }
+}
